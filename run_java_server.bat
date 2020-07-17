373769701254fed307fac4c149ddeae62a16ec32
Brigham Andersen <brighamband@gmail.com>
2020-07-17 16:38:54 -0600

    moved areed client out of personal folder

--- run_java_server.bat
+++ run_java_server.bat
@@ -1,3 +1,3 @@
-cd "C:\Coding\SF\silverfund\interactive-broker-python-api\resources\clientportal.beta.gw"
+cd "C:\Coding\SF\interactive-broker-python-api\resources\clientportal.beta.gw"
 cls
 "bin/run.bat" "root/conf.yaml"
